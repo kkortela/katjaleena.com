fb8ed82c2a5a27da349c8e3bac0fd29be7b2d48e
Mika Väisänen <mika.vaisanen@reaktor.com>
2017-02-15 01:12:30 +0200

    Move case images to config file

--- app/components/Cases.js
+++ app/components/Cases.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './cases.css'
+import images from '../config'
 
 const Cases = () => {
   const Case = ({img, title, text}, i) => {
@@ -17,28 +18,6 @@ const Cases = () => {
       </div>
     )
   }
-  const images = [
-    {
-      img: require(`../../public/images/amma.jpg`),
-      title: 'Amma',
-      text: 'Case project for building an app for asthma tracking',
-    },
-    {
-      img: require(`../../public/images/instashop.jpg`),
-      title: 'Instashop',
-      text: 'Responsive online grocery store websites',
-    },
-    {
-      img: require(`../../public/images/wedding.jpg`),
-      title: 'Wedding website',
-      text: 'Responsive wedding website',
-    },
-    {
-      img: require(`../../public/images/dignity-meals.jpg`),
-      title: 'Dignity Meals',
-      text: 'Mobile app for connecting food donors with charities',
-    },
-  ]
   return (
       <div className={styles.cases}>
         <div className="pure-g">
---
+++ app/config.js
@@ -0,0 +1,24 @@
+const images = [
+  {
+    img: require(`../public/images/amma.jpg`),
+    title: 'Amma',
+    text: 'Case project for building an app for asthma tracking',
+  },
+  {
+    img: require(`../public/images/instashop.jpg`),
+    title: 'Instashop',
+    text: 'Responsive online grocery store websites',
+  },
+  {
+    img: require(`../public/images/wedding.jpg`),
+    title: 'Wedding website',
+    text: 'Responsive wedding website',
+  },
+  {
+    img: require(`../public/images/dignity-meals.jpg`),
+    title: 'Dignity Meals',
+    text: 'Mobile app for connecting food donors with charities',
+  },
+]
+
+export default images
