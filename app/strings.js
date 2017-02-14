6e4dd355d852cae256019d831f36ba6160dbce45
Mika Väisänen <mika.vaisanen@reaktor.com>
2017-02-15 01:16:00 +0200

    Add Works page strings

--- app/strings.js
+++ app/strings.js
@@ -2,6 +2,10 @@ const strings = {
   intro: {
     title: 'Welcome!',
     text: 'I’m Katja, a UX/UI designer, who designs for a reason and wants to improve people’s lives.'
+  },
+  work: {
+    title: 'My Work',
+    text: 'Here’s some of my latest work'
   }
 }
 
