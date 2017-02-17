0dc1f712236a8774b2db7d94e694f88a9e780bc2
Katja Kortelainen <kkortela@Katja-MacBook-Air.local>
2017-02-17 14:14:11 +0200

    Added strings and image to About me

--- app/components/About.js
+++ app/components/About.js
@@ -4,6 +4,11 @@ import styles from './about.css'
 const About = ({strings}) => {
   return (
     <div className={styles.about}>
+      <img src={require(`../../public/images/aboutme.jpg`)} />
+      <h1>{strings.intro.title}</h1>
+      <p>{strings.intro.text1}</p>
+      <p>{strings.intro.text2}</p>
+      <p>{strings.intro.text3}</p>
       <h1>{strings.references.title}</h1>
       <div className={`pure-g ${styles.references}}`}>
         <div className={`pure-u-1 pure-u-md-1-3 ${styles.wrapper}`}>
