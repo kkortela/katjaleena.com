931450505d45fa9b33c6b6ccec515774e44f1b63
Katja Kortelainen <kkortela@Katja-MacBook-Air.local>
2017-02-18 22:32:14 +0200

    Changed the body font in About Me

--- app/components/About.js
+++ app/components/About.js
@@ -4,11 +4,17 @@ import styles from './about.css'
 const About = ({strings}) => {
   return (
     <div className={styles.about}>
-      <img src={require(`../../public/images/aboutme.jpg`)} />
-      <h1>{strings.intro.title}</h1>
-      <p>{strings.intro.text1}</p>
-      <p>{strings.intro.text2}</p>
-      <p>{strings.intro.text3}</p>
+      <div className={`pure-g ${styles.padding}}`}>
+        <div className="pure-u-lg-1-2">
+          <img src={require(`../../public/images/aboutme.jpg`)} />
+        </div>
+        <div className={`pure-u-lg-1-2 ${styles.aboutme}}`}>
+          <h1>{strings.intro.title}</h1>
+          <p className={styles.aboutme}>{strings.intro.text1}</p>
+          <p className={styles.aboutme}>{strings.intro.text2}</p>
+          <p className={styles.aboutme}>{strings.intro.text3}</p>
+        </div>
+      </div>
       <h1>{strings.references.title}</h1>
       <div className={`pure-g ${styles.references}}`}>
         <div className={`pure-u-1 pure-u-md-1-3 ${styles.wrapper}`}>
