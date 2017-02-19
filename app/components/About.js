c4260ce9d75356a1f22459628356364a0f64a654
Katja Kortelainen <kkortela@Katja-MacBook-Air.local>
2017-02-19 23:44:57 +0200

    Hid the references in About me

--- app/components/About.js
+++ app/components/About.js
@@ -21,37 +21,39 @@ const About = ({strings}) => {
           </div>
         </div>
       </div>
-      <h1>{strings.references.title}</h1>
-      <div className={`pure-g ${styles.references}}`}>
-        <div className={`pure-u-1 pure-u-md-1-3 ${styles.wrapper}`}>
-          <div className={styles.referenceBlock}>
-            <div>
-              <p>{strings.references.ref1.text}</p>
-              <span>- {strings.references.ref1.referer}</span>
-            </div>
-            <div>
-              <p>{strings.references.ref2.text}</p>
-              <span>- {strings.references.ref2.referer}</span>
-            </div>
-          </div>
-        </div>
-        <div className={`pure-u-1 pure-u-md-1-3 ${styles.wrapper}`}>
-          <div className={styles.referenceBlock}>
-            <div>
-              <p>{strings.references.ref3.text}</p>
-              <span>- {strings.references.ref3.referer}</span>
-            </div>
-            <div>
-              <p>{strings.references.ref4.text}</p>
-              <span>- {strings.references.ref4.referer}</span>
+      <div style={{display:'none'}}>
+        <h1>{strings.references.title}</h1>
+        <div className={`pure-g ${styles.references}}`}>
+          <div className={`pure-u-1 pure-u-md-1-3 ${styles.wrapper}`}>
+            <div className={styles.referenceBlock}>
+              <div>
+                <p>{strings.references.ref1.text}</p>
+                <span>- {strings.references.ref1.referer}</span>
+              </div>
+              <div>
+                <p>{strings.references.ref2.text}</p>
+                <span>- {strings.references.ref2.referer}</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={`pure-u-1 pure-u-md-1-3 ${styles.wrapper}`}>
-          <div className={styles.referenceBlock}>
-            <div>
-              <p>{strings.references.ref5.text}</p>
-              <span>- {strings.references.ref5.referer}</span>
+          <div className={`pure-u-1 pure-u-md-1-3 ${styles.wrapper}`}>
+            <div className={styles.referenceBlock}>
+              <div>
+                <p>{strings.references.ref3.text}</p>
+                <span>- {strings.references.ref3.referer}</span>
+              </div>
+              <div>
+                <p>{strings.references.ref4.text}</p>
+                <span>- {strings.references.ref4.referer}</span>
+              </div>
+            </div>
+          </div>
+          <div className={`pure-u-1 pure-u-md-1-3 ${styles.wrapper}`}>
+            <div className={styles.referenceBlock}>
+              <div>
+                <p>{strings.references.ref5.text}</p>
+                <span>- {strings.references.ref5.referer}</span>
+              </div>
             </div>
           </div>
         </div>
